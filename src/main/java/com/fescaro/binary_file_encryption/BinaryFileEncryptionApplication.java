b84ac7bd535835268f9b0b330a334aa5a402425d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-29 17:14:15 +0900

    🎉 Project init

---
+++ src/main/java/com/fescaro/binary_file_encryption/BinaryFileEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.fescaro.binary_file_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryFileEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryFileEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fescaro/binary_file_encryption/BinaryFileEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fescaro.binary_file_encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryFileEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
