dece3e785844b2852e3331cfb60d63857f0a7eb1
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-30 18:19:20 +0900

    🎨 [add] CustomUserDetails 작성, User 엔티티에 role 속성 추가

---
+++ src/main/java/com/fescaro/binary_file_encryption/global/jwt/dto/CustomUserDetails.java
@@ -0,0 +1,33 @@
+package com.fescaro.binary_file_encryption.global.jwt.dto;
+
+import com.fescaro.binary_file_encryption.domain.member.entity.User;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) () -> user.getRole());
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public Long getId(){ return user.getId(); }
+}
