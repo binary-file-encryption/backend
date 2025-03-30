0bdab5fcd6f0d862357a605afcd1cd83f8f2884d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-30 18:12:17 +0900

    🎨 [add] member 엔티티 작성

---
+++ src/main/java/com/fescaro/binary_file_encryption/domain/member/entity/Member.java
@@ -0,0 +1,21 @@
+package com.fescaro.binary_file_encryption.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
