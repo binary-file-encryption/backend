f55180036f8143b370264aa492ccbf5b715b84e7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-03-30 18:25:50 +0900

    ✨ [feat] CustomUserDetailsService 작성
    username을 바탕으로 회원 엔티티 조회, CustomUserDetials 객체 생성후 반환

---
+++ src/main/java/com/fescaro/binary_file_encryption/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fescaro.binary_file_encryption.domain.user.repository;
+
+import com.fescaro.binary_file_encryption.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+}
